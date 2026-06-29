2e129f4f2987ea328134b0bcb1aec404dfff92d0
Yves Bertot <yves.bertot@inria.fr>
2026-06-29 12:50:28 +0200

    removed a lot of debugging stuff, and still in the process of debugging repetition that fails

--- tester.v
+++ tester.v
@@ -1,5 +1,6 @@
 From Stdlib Require Import Reals Lra.
 Import Ring_polynom.
+From elpi Require Import elpi ext.
 From elpi.ext Require Import flipper.
 Open Scope R_scope.
 Definition RField_lemma5 :=
@@ -7,44 +8,32 @@ Definition RField_lemma5 :=
   (@f_equal _ _ Rinv) (F2AF (Eqsth R) (Eq_ext _ _ _) Rfield) R_rm R_power_theory
   get_signZ_th (Ztriv_div_th Rset IZR).
 
-
-Ltac compute_gcd D N :=
-  constr:(pair 1%Z (pair (PX (Pc 2) 1 (Pc 0))
-    (pair (Pc 1) (PX (Pc 1) 1 (Pc 0))))%Z).
-
-
-Ltac compute_gcd' D N :=
-gcd_for_field N D;
-match goal with |- wrapped_result (?F, (?N',(?D', ?Gcd'))) -> _
-=> intros _;
-  constr:(pair F (pair N' (pair D' Gcd')))
-    end.
-
-Ltac den_gcd_n0 :=
-  split;[ apply Rmult_integral_contrapositive; split;
-    [apply not_eq_sym, Rlt_not_eq, Rlt_0_2 | ]| ]; apply PI_neq0.
-
-Ltac find_fraction dummy :=
+Ltac find_fraction :=
   let t_eq := fresh "term_eq" in
   let hyp := fresh "rewrite_lemma" in
   intros t_eq hyp;
   lazymatch type of t_eq with
   | ?term = _ =>
     clear t_eq;
+    idtac "in find_fraction" term;
     lazymatch type of hyp with
     | forall _ _ _ _,
       gcd_cond _ _ _ _ _ _ _ _ _ _ _ _ _ _ ?FV ?D _ ?N _ _ -> _
       =>
     let D1 := eval vm_compute in D in
     let N1 := eval vm_compute in N in
+    idtac "debug1" D1 N1;
     let hyp_aux := fresh "gcd_cond_proof" in
     let fact_n0 := fresh "factor_not_0" in
     let num_eq := fresh "equality_for_numerator" in
     let den_eq := fresh "equality_for_denumerator" in
-      gcd_for_field N1 D1;
-      lazymatch goal with
-        |- wrapped_result (?F ,(?N2,(?D2, ?Gcd))) -> _
-        => intros _;
+      let res := constr:(ltac:(elpi factorize_by_gcd ltac_term:(N1) 
+          ltac_term:(D1))) in
+      idtac "debug5" res;
+      let F := eval cbv [fst] in (fst res) in
+      let N2 := eval cbv [fst snd] in (fst (snd res)) in
+      let D2 := eval cbv [fst snd] in (fst (snd (snd res))) in
+      let Gcd := eval cbv [snd fst] in (snd (snd (snd res))) in
         assert (fact_n0 : IZR F <> 0) by (apply eq_IZR_contrapositive; easy);
         enough (gcd_cond 0 1 Rplus Rmult Rminus Ropp eq 0%Z 1%Z Z.eqb IZR
           BinNat.N.to_nat
@@ -53,7 +42,7 @@ Ltac find_fraction dummy :=
           Pmul 0%Z 1%Z Z.add Z.mul Z.eqb N2 Gcd /\
           Pmul 0%Z 1%Z Z.add Z.mul Z.eqb (Pc F) D =
           Pmul 0%Z 1%Z Z.add Z.mul Z.eqb D2 Gcd)) as [hyp_aux [num_eq den_eq]];
-        [
+        [ idtac "one goal";
           generalize (hyp F N2 D2 Gcd hyp_aux fact_n0 num_eq den_eq);
           clear hyp_aux hyp fact_n0 num_eq den_eq;
           let hyp2 := fresh "rewrite_lemma2" in 
@@ -63,49 +52,63 @@ Ltac find_fraction dummy :=
             change t with term in hyp2;
             (rewrite hyp2; clear hyp2);
             [ unfold display_pow_linear; reduce_Pphi_pow|
-             cbv [PCond condition PEeval BinList.nth BinNat.N.to_nat
+             cbv [fst snd PCond condition PEeval BinList.nth BinNat.N.to_nat
                   List.hd PosDef.Pos.to_nat Init.Nat.add PosDef.Pos.iter_op]]
           end
-          |
+          | idtac "other goal";
           clear hyp fact_n0;
-          split;       
+          (split;       
            [split; 
             [
               match goal with 
               | |-  (_ -> _)  =>
               intros [? [? ?]]; easy
-               | |- ?G  => try easy; intros [?[H H1]]; apply Z.eqb_eq in H1; unfold norm_subst in H; simpl in H; subst; easy
+               | |- ?G  => try easy; intros [?[H H1]]; apply Z.eqb_eq in H1; unfold norm_subst in H; simpl in H; subst; try easy
               end
             | reduce_Pphi_pow
             ] 
-          | easy || fail 1000 "polynomial equalities should have been proved"]
+          | easy || fail 1000 "polynomial equalities should have been proved"])
+            || fail 1000 "failed to prove other goal"
        ]
-        | |-?G => idtac G; fail 1000 "find_fraction fail"
-      end
     end
   end.
 
 Ltac fs5 := Field_simplify_gcd RField_lemma5 ltac:(find_fraction).
 
-Lemma toto : PI / (PI ^ 2 + PI ^ 2) = 4 / 4 * PI.
-(* find_fraction (). *)
-field_simplify_gcd fs5  / (PI / (PI ^ 2 + PI ^ 2)).
-Fail field_simplify_gcd fs5 / (4 / (4 * PI)).
-admit.
-admit.
-enough (PI > 0) by lra.
+Lemma happy_life : PI / (PI ^ 2 + PI ^ 2) = 4 / (8 * PI).
+Proof.
+field.
+enough (PI > 0) by nra.
 apply PI_RGT_0.
-split.
-  lra.
-  admit.
-  Unshelve.
-  compute.
-Admitted.
- Lemma toto3 x : (3*x + 6 )/3 = x+2.
- field_simplify_gcd fs5  / ((3*x + 6 )/3).
-easy.
-Admitted.
+Qed.
 
-Lemma toto4 x :(x + 2) /(3/3) = x + 2.
- field_simplify_gcd fs5  / ((x + 2) /(3/3)).
-Admitted.
+Lemma field_unhappy : 1 + exp (PI / (PI ^ 2 + PI ^ 2)) =
+  exp (4 / (8 * PI)) + 1.
+Proof.
+Fail field.
+Abort.
+
+Lemma ring_happy : 1+ exp (PI ^ 2 - 1) = exp ((PI + 1) * (PI - 1)) + 1.
+Proof.
+ring_simplify (PI ^ 2 - 1) ((PI + 1) * (PI - 1)).
+ring.
+Qed.
+
+Lemma field_still_unhappy :
+  exp (PI / (PI ^ 2 + PI ^ 2)) = exp (4 / (8 * PI)).
+Proof.
+assert (PI_GT0 := PI_RGT_0).
+field_simplify (PI / (PI ^ 2 + PI ^ 2)) (4 / (8 * PI)).
+Abort.
+
+Lemma field_solution :
+  exp (PI / (PI ^ 2 + PI ^ 2)) = exp (4 / (8 * PI)).
+Proof.
+assert (PI_GT0 := PI_RGT_0).
+
+field_simplify_gcd fs5  / (PI / (PI ^ 2 + PI ^ 2)) (4 / (8 * PI)).
+Fail field.
+field_simplify_gcd fs5 / (4 / (8 * PI)).
+field.
+all: try nra.
+Qed.
--- theories/flipper.v
+++ theories/flipper.v
@@ -8,35 +8,8 @@ Import List.
 Elpi Command test.
 Elpi Accumulate Plugin "ext.elpi".
 Open Scope R_scope.
-Parameter x : R.
-Definition x1 := (x^5 + 4*x^3 + 5*x^2 + 3*x + 15).
-Definition x2 := (x^4 - x^3 + 2*x^2 - 3*x - 3).
-Notation "x + y" := (@FEadd Z x y).
-Notation "x - y" := (@FEsub Z x y).
-Notation "x * y" := (@FEmul Z x y).
-Notation "`C[ n ]" := (@FEc Z n).
-Notation "x ^ y" := (@FEpow Z x (Npos y%positive)).
-Notation "`V[ n ]" := (@FEX Z (n%positive)).
-Ltac foo z l :=
-    let y := mkFieldexpr Z IZR_tac Rpow_tac  0 1 Rplus Rmult Rminus Ropp Rdiv Rinv pow z (l: list R) in
-     assert (y = y).
-Definition wrapper_F (x : FExpr Z) :=
-True.
-Definition wrapper_LR (x : list R) :=
-True.
 
-Definition Peeval' := @PEeval R 0 1 Rplus Rmult Rminus Ropp Z IZR nat BinNat.N.to_nat pow.
-Definition Feeval' := @FEeval R 0 1 Rplus Rmult Rminus Ropp Rdiv Rinv Z IZR nat BinNat.N.to_nat pow.
 Elpi Accumulate File encode.
-Elpi Query lp:{{
-collect_glob_lcm {{`V[ 1] ^ 5 + `C[ 10%Z] * `V[ 1] ^ 3 + `C[ 5%Z] * `V[ 1] ^ 2 + `C[ 3%Z] * `V[ 1] + `C[ 15%Z]}} 1 X.
-}}.
-
-Definition wrapper_PolZ (x : Pol Z) :=
-True.
-
-Definition wrapped_result (t: Z *  (Pol Z * (Pol Z * Pol Z))) :=
-True.
 
 Definition norm := norm_aux 0%Z 1%Z Z.add Z.mul Z.sub Z.opp Z.eqb.
 
@@ -50,132 +23,14 @@ solve (goal _ _ _ _ [trm N, trm D] as G) GL :-
   coq.reduction.vm.norm {{norm lp:N'}} {{Pol Z}} N'',
   coq.reduction.vm.norm {{norm lp:D'}} {{Pol Z}} D'',
   coq.reduction.vm.norm {{norm lp:Gcd}} {{Pol Z}} Gcd',
-  (refine {{_  (I : (wrapped_result (pair lp:LCM (pair lp:N'' (pair lp:D'' lp:Gcd')))))}} G GL).
+  (refine {{pair lp:LCM (pair lp:N'' (pair lp:D'' lp:Gcd'))}} G GL).
 
-
-solve (goal _ _ {{wrapper_LR lp:L -> wrapper_F lp:D ->
-          wrapper_F lp:N -> _}} _ _ as G ) GL :-
-  coq.say "second variant",
-  gcd_and_factors fe_encode fe_decode N D N' D' Gcd LCM,
-  (refine
-  {{let H : (1%R/(IZR lp:LCM) *(Feeval' lp:L lp:N))%R = Feeval' lp:L (@FEmul Z lp:N' lp:Gcd) := _ in
-  let H' : (1%R/(IZR lp:LCM) *(Feeval' lp:L lp:D))%R = Feeval' lp:L (@FEmul Z lp:D' lp:Gcd) := _ in _}}
-  G GL)
-.
 }}.
-
-Ltac gcd_for_field pol1 pol2 :=
-  elpi factorize_by_gcd ltac_term:(pol1) ltac_term:(pol2);
-  match goal with  |-
-      wrapped_result (?Co, (?N',(?D', ?Gcd'))) ->_
- => idtac
- end.
-
-Open Scope R_scope.
-
-Ltac reify_then_call_factorize_by_gcd n d :=
-let l := FFV IZR_tac Rpow_tac 0 1 Rplus Rmult Rminus Ropp Rdiv Rinv pow n (nil(A:=R)) in
-let l' := FFV IZR_tac Rpow_tac 0 1 Rplus Rmult Rminus Ropp Rdiv Rinv pow d l in
-    let n' := mkFieldexpr Z IZR_tac Rpow_tac  0 1 Rplus Rmult Rminus Ropp Rdiv Rinv pow n l' in
-    let d' := mkFieldexpr Z IZR_tac Rpow_tac  0 1 Rplus Rmult Rminus Ropp Rdiv Rinv pow d l' in
-    generalize (I:wrapper_F n');
-    generalize  (I:wrapper_F d');
-    generalize (I:wrapper_LR l');
-    simpl BinNat.N.of_nat;
-    lazymatch goal with
-      |- wrapper_LR ?l'' -> wrapper_F ?d'' -> wrapper_F ?n'' -> _ =>
-      (elpi factorize_by_gcd n'' d''); intros _ _ _
-      end.
-Close Scope R_scope.
-
-Ltac consume_gcd pol1 pol2 :=
-let v := (gcd_for_field pol1 pol2) in
-let w := constr:(v) in
-assert (w = w) by easy.
-
-
-Print Pphi_pow.
-Definition back_to_R := Pphi_pow 0%R 1%R Rplus Rmult Rminus Ropp
-  0%Z 1%Z Z.eqb IZR BinNat.N.to_nat pow (fun _ => None).
 
 (* TODO: find how to reduce Pphi_pow without reducing IZR. *)
 Ltac reduce_Pphi_pow :=
-    cbv [Pphi_pow Pphi_avoid mult_dev Peq Z.eqb P0 mkmult_c
+    cbv [fst snd Pphi_pow Pphi_avoid mult_dev Peq Z.eqb P0 mkmult_c
         mkmult_c_pos get_signZ Pos.eqb mkmult_rec List.rev' add_pow_list
         mkmult1 List.rev_append List.hd BinNat.N.add
         add_mult_dev mkadd_mult mkmult_c_pos mkmult_rec BinNat.N.to_nat PosDef.Pos.to_nat PosDef.Pos.iter_op List.rev' List.rev_append  List.hd List.tl add_pow_list mkmult_rec Pos.add Nat.add].
 
-Check back_to_R.
-
-Goal True.
-gcd_for_field (PX (Pc (1%Z)) 2%positive (Pc (-1)%Z))
- (PX (Pc (1%Z)) 1%positive (Pc 1%Z)).
-let in1 := constr:(PX (Pc (1%Z)) 2%positive (Pc (- 1)%Z)) in
-  let v_in1 := constr:(back_to_R (PI::nil) in1) in
-  generalize (eq_refl v_in1);
-  unfold back_to_R.
-reduce_Pphi_pow.
-intros v_in1.
-match goal with |- wrapped_result (_, (?out1, (?out2, ?gcd))) -> _ =>
-  generalize (eq_refl (back_to_R (PI::nil) out1));
-  unfold back_to_R;
-  let name1 := fresh "out1" in
-  reduce_Pphi_pow; intros name1
-end.
-let in2 := constr:(PX (Pc (1%Z)) 1%positive (Pc 1%Z)) in
-  let v_in2 := constr:(back_to_R (PI::nil) in2) in
-  generalize (eq_refl v_in2);
-  unfold back_to_R.
-reduce_Pphi_pow.
-intros v_in2.
-match goal with |- wrapped_result (_, (?out1, (?out2, ?gcd))) -> _ =>
-  generalize (eq_refl (back_to_R (PI::nil) out2));
-  unfold back_to_R;
-  let name := fresh "out2" in
-  reduce_Pphi_pow; intros name
-end.
-
-easy.
-Qed.
-
-Ltac simplify_by_gcd n d :=
-  unshelve reify_then_call_factorize_by_gcd n d;
-  cbv [Peeval' PEeval BinList.nth BinNat.N.to_nat List.hd PosDef.Pos.to_nat PosDef.Pos.iter_op Init.Nat.add];
-  try ring.
-
-Goal True.
- simplify_by_gcd (PI^5 + 4* PI^3 + 5* PI^2 +3* PI + 15)%R (PI^4 - PI ^3 + 2 *PI^2 - 3 * PI -3)%R; cbv [Feeval' FEeval BinList.nth BinNat.N.to_nat List.hd PosDef.Pos.to_nat PosDef.Pos.iter_op Init.Nat.add]; try field.
-
-easy.
-Qed.
-Notation "x + y" := (@PEadd Z x y).
-Notation "x - y" := (@PEsub Z x y).
-Notation "x * y" := (@PEmul Z x y).
-Notation "`C[ n ]" := (@PEc Z n).
-Notation "x ^ y" := (@PEpow Z x (Npos y%positive)).
-Notation " - y" := (@PEopp Z y ).
-Notation "'x" := (@PEX Z 1%positive).
-Notation "'y" := (@PEX Z 2%positive).
-Notation "'z" := (@PEX Z 3%positive).
-
-Definition p1 := 'x ^ 2 + (`C[ 3%Z ]) *'y ^ 3 - 'z .
-Definition pol1 := norm p1.
-
-Definition p2 := ('x ^ 2 + 'y) * (`C[ 3%Z ] *'y ^ 4 - 'x ^ 3).
-Definition pol2 := norm p2.
-
-Definition p3 := ('x ^ 3 + 'y) * ('y  - 'x ^ 3).
-Definition pol3 := norm p3.
-
-Definition p4 := (norm_subst 0%Z 1%Z Z.add Z.mul Z.sub Z.opp Z.eqb Z.quotrem ring_subst_niter nil
-(PEadd (PEpow 'x (Npos 2)) (PEpow 'x (Npos 2)))).
-Definition pol4 :=  PX (Pc 2%Z) 2 (Pc 0%Z).
-Check pol4.
-Definition p5 := (norm_subst 0%Z 1%Z Z.add Z.mul Z.sub Z.opp Z.eqb Z.quotrem ring_subst_niter nil('x)).
-Definition pol5 := PX (Pc 1%Z) 1 (Pc 0%Z).
-Elpi Accumulate File encode.
-
-Goal True.
-gcd_for_field (PX (Pc 2%Z) 2 (Pc 0%Z)) (PX (Pc 1%Z) 1 (Pc 0%Z)).
-easy.
-Qed.
