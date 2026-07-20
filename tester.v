c6052b4295e1fb1ad30f8b9549464ef806c86a25
Yves Bertot <Yves.Bertot@inria.fr>
2026-07-20 17:08:55 +0200

    moved the finishing tactic from the test file to the file in theories/

--- tester.v
+++ tester.v
@@ -3,62 +3,6 @@ Import Ring_polynom.
 From elpi Require Import elpi ext.
 From elpi.ext Require Import flipper.
 Open Scope R_scope.
-Definition RField_lemma5 :=
-  Field_theory.Field_rw_pow_correct_w_gcd (Eqsth R) (Eq_ext Rplus Rmult Ropp)
-  (@f_equal _ _ Rinv) (F2AF (Eqsth R) (Eq_ext _ _ _) Rfield) R_rm R_power_theory
-  get_signZ_th (Ztriv_div_th Rset IZR).
-  
-Definition Pmul := Pmul 0%Z 1%Z Z.add Z.mul Z.eqb.
-Local Notation "x ?== y" := (Peq Z.eqb x y) (at level 70, no associativity).
-
-(* Term is the expression that was given by the user for simplification.
-  FV is the list of sub-expressions of Term that are not recognized as
-  compound field expression (they are considered as variables).  D and N
-  are two polynomials (in type Pol Z), such that Term = N / D is already
-  proved,  but N / D is not a reduced fraction because these two polynomials
-  may have a non-trivial common divisor.
-  This tactic also assume that the goal has approximately the shape :
-  forall nfe, Fnorm FV fe = nfe ->  <some conditions> ->
-    FEeval _ .. _ fe -> Pphi_pow N / Pphi_pow D
-  where FEeval _ .. _ fe is convertible with Term.  *)
-Ltac find_fraction Term FV D N :=
-  let hyp := fresh "rewrite_lemma" in
-  intros hyp;
-  let D1 := eval vm_compute in D in
-  let N1 := eval vm_compute in N in
-  let fact_n0 := fresh "factor_not_0" in
-  let num_eq := fresh "equality_for_numerator" in
-  let den_eq := fresh "equality_for_denumerator" in
-  let res :=
-    constr:(ltac:(elpi factorize_by_gcd ltac_term:(N1) ltac_term:(D1))) in
-  let F := eval cbv [fst] in (fst res) in
-  let N2 := eval cbv [fst snd] in (fst (snd res)) in
-  let D2 := eval cbv [fst snd] in (fst (snd (snd res))) in
-  let Gcd := eval cbv [snd fst] in (snd (snd (snd res))) in
-  assert (fact_n0 : IZR F <> 0) by (apply eq_IZR_contrapositive; easy);
-  enough
-  ((Pmul (Pc F) N1 ?== Pmul N2 Gcd) = true /\
-    (Pmul (Pc F) D1 ?== Pmul D2 Gcd) = true) as [num_eq den_eq];
-    [generalize (hyp F N2 D2 Gcd fact_n0 num_eq den_eq);
-          clear hyp fact_n0 num_eq den_eq;
-          let hyp2 := fresh "rewrite_lemma2" in 
-          intros hyp2;
-          let ty := type of hyp2 in
-          lazymatch type of hyp2 with
-          | _ -> ?t = ?r =>
-            change t with Term in hyp2;
-            (rewrite hyp2; clear hyp2);
-            [ unfold display_pow_linear; reduce_Pphi_pow|
-             cbv [fst snd PCond condition PEeval BinList.nth BinNat.N.to_nat
-            List.hd PosDef.Pos.to_nat Init.Nat.add PosDef.Pos.iter_op
-            BinList.jump List.tl]]
-          end
-          |
-          clear hyp fact_n0;
-          split; 
-           [ reduce_Pphi_pow; easy
-          |   easy]
-  ].
 
 Lemma happy_life : PI / (PI ^ 2 + PI ^ 2) = 4 / (8 * PI).
 Proof.
@@ -71,12 +15,10 @@ Lemma field_unhappy : 1 + exp (PI / (PI ^ 2 + PI ^ 2)) =
   exp (4 / (8 * PI)) + 1.
 Proof.
 Fail field.
-Abort.
-
-Definition Nnorm :=
-  norm_subst (0%Z) (1%Z) Z.add Z.mul Z.sub Z.opp Z.eqb Z.div_eucl.
-
-Ltac fs5 := Field_simplify_gcd Nnorm RField_lemma5 ltac:(find_fraction).
+assert (PI_GT0 := PI_RGT_0).
+replace (PI / (PI ^ 2 + PI ^ 2)) with (4 / (8 * PI)) by (field; nra).
+ring.
+Qed.
 
 Lemma field_still_unhappy :
   exp (PI / (PI ^ 2 + PI ^ 2)) = exp (4 / (8 * PI)).
@@ -105,6 +47,27 @@ field_simplify (2 * (PI / 4)).
 field_simplify_gcd fs5 / (2 * PI / 4);[ | nra ..].
 easy.
 Qed.
+
+Lemma polynomial_division x : 0 < x -> 
+  exp ((x ^  2 - /((4 - 1) / 3)) / (x + 1)) = exp (x - 1).
+Proof.
+intros xgt0.
+(* field_simplify (x ^ 2 - /((4 - 1)/3)). *)
+field_simplify_gcd fs5 / ((x ^ 2 - /((4 - 1)/3)) / (x + 1)).
+field.
+nra.
+Qed.
+
+Elpi Tactic remove_trivial_non_zero.
+
+Elpi Accumulate lp:{{
+solve (goal _ _ _ _ _ as G) GL :-
+  gcd_and_factors N D N' D' Gcd LCM,
+  coq.reduction.vm.norm {{norm lp:N'}} {{Pol Z}} N'',
+  coq.reduction.vm.norm {{norm lp:D'}} {{Pol Z}} D'',
+  coq.reduction.vm.norm {{norm lp:Gcd}} {{Pol Z}} Gcd',
+  (refine {{pair lp:LCM (pair lp:N'' (pair lp:D'' lp:Gcd'))}} G GL).
+}}.
 
 Elpi Tactic toto.
 From elpi.ext Extra Dependency "encode.elpi" as encode.
@@ -141,4 +104,9 @@ Elpi Query lp:{{
   coq.term->string Gcd' Gcds,
   coq.term->string LCMZ LCMs
   % gcd_and_factors pol_encode pe_decode V1 V2 A B C M
+}}.
+
+Elpi Query lp:{{
+  pe_decode (mul (var 1) (var 1)) C,
+  coq.term->string C S.
 }}.
--- theories/flipper.v
+++ theories/flipper.v
@@ -19,16 +19,71 @@ Elpi Accumulate File encode.
 Elpi Accumulate lp:{{
 
 solve (goal _ _ _ _ [trm N, trm D] as G) GL :-
-  gcd_and_factors pol_encode pe_decode N D N' D' Gcd LCM,
+  gcd_and_factors N D N' D' Gcd LCM,
   coq.reduction.vm.norm {{norm lp:N'}} {{Pol Z}} N'',
   coq.reduction.vm.norm {{norm lp:D'}} {{Pol Z}} D'',
   coq.reduction.vm.norm {{norm lp:Gcd}} {{Pol Z}} Gcd',
   (refine {{pair lp:LCM (pair lp:N'' (pair lp:D'' lp:Gcd'))}} G GL).
 }}.
 
+Definition RField_lemma5 :=
+  Field_theory.Field_rw_pow_correct_w_gcd (Eqsth R) (Eq_ext Rplus Rmult Ropp)
+  (@f_equal _ _ Rinv) (F2AF (Eqsth R) (Eq_ext _ _ _) Rfield) R_rm R_power_theory
+  get_signZ_th (Ztriv_div_th Rset IZR).
+
+Definition Pmul := Pmul 0%Z 1%Z Z.add Z.mul Z.eqb.
+Local Notation "x ?== y" := (Peq Z.eqb x y) (at level 70, no associativity).
+
+Definition Nnorm :=
+  norm_subst (0%Z) (1%Z) Z.add Z.mul Z.sub Z.opp Z.eqb Z.div_eucl.
+
 (* TODO: find how to reduce Pphi_pow without reducing IZR. *)
 Ltac reduce_Pphi_pow :=
-    cbv [fst snd Pphi_pow Pphi_avoid mult_dev Peq Z.eqb P0 mkmult_c
-        mkmult_c_pos get_signZ Pos.eqb mkmult_rec List.rev' add_pow_list
-        mkmult1 List.rev_append List.hd BinNat.N.add
-        add_mult_dev mkadd_mult mkmult_c_pos mkmult_rec BinNat.N.to_nat PosDef.Pos.to_nat PosDef.Pos.iter_op List.rev' List.rev_append  List.hd List.tl add_pow_list mkmult_rec Pos.add Nat.add].
+  cbv [fst snd Pphi_pow Pphi_avoid mult_dev Peq Z.eqb P0 mkmult_c
+      mkmult_c_pos get_signZ Pos.eqb mkmult_rec List.rev' add_pow_list
+      mkmult1 List.rev_append List.hd BinNat.N.add
+      add_mult_dev mkadd_mult mkmult_c_pos mkmult_rec BinNat.N.to_nat 
+      PosDef.Pos.to_nat PosDef.Pos.iter_op List.rev' List.rev_append
+      List.hd List.tl add_pow_list mkmult_rec Pos.add Nat.add].
+
+Ltac reduce_PCond :=
+  cbv [fst snd PCond condition PEeval BinList.nth BinNat.N.to_nat
+        List.hd PosDef.Pos.to_nat Init.Nat.add PosDef.Pos.iter_op
+        BinList.jump List.tl].
+
+Ltac Real_const_not_0 :=
+  apply eq_IZR_contrapositive; easy.
+
+(* Term is the expression that was given by the user for simplification.
+  FV is the list of sub-expressions of Term that are not recognized as
+  compound field expression (they are considered as variables).  D and N
+  are two polynomials (in type Pol Z), such that Term = N / D is already
+  proved,  but N / D is not a reduced fraction because these two polynomials
+  may have a non-trivial common divisor.
+  This tactic also assume that the goal has approximately the shape :
+  forall nfe, Fnorm FV fe = nfe ->  PCond <some list> ->
+    FEeval _ .. _ fe -> Pphi_pow N / Pphi_pow D
+  where FEeval _ .. _ fe is convertible with Term.  *)
+Ltac fraction_finisher Term FV D N :=
+let hyp := fresh "rewrite_lemma" in intros hyp;
+let D1 := eval vm_compute in D in
+let N1 := eval vm_compute in N in
+let eq1 := fresh "eq1" in let eq2 := fresh "eq2" in
+let res :=
+  constr:(ltac:(elpi factorize_by_gcd ltac_term:(N1) ltac_term:(D1))) in
+let F := eval cbv [fst] in (fst res) in
+let N2 := eval cbv [fst snd] in (fst (snd res)) in
+let D2 := eval cbv [fst snd] in (fst (snd (snd res))) in
+let Gcd := eval cbv [snd fst] in (snd (snd (snd res))) in
+let hyp2 := fresh "rew_l2" in
+assert (hyp2 := hyp F N2 D2 Gcd);
+lazymatch type of hyp2 with
+| _ -> _ -> _ -> _ -> ?t = ?r =>
+change t with Term in hyp2;
+  (try rewrite hyp2; clear hyp hyp2);
+  [ unfold display_pow_linear; reduce_Pphi_pow|
+    easy| easy | easy| reduce_PCond]
+end.
+
+
+Ltac fs5 := Field_simplify_gcd Nnorm RField_lemma5 ltac:(fraction_finisher).
