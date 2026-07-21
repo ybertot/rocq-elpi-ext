e41501490761b4f35f197d59ecb903fefc4cce20
Yves Bertot <Yves.Bertot@inria.fr>
2026-07-21 14:49:07 +0200

    checkpoint

--- theories/flipper.v
+++ theories/flipper.v
@@ -4,14 +4,7 @@ From elpi.ext Extra Dependency "encode.elpi" as encode.
 
 From Stdlib Require Import Field Ring_polynom Reals.
 Import List.
-
-Elpi Command test.
-Elpi Accumulate Plugin "ext.elpi".
 Open Scope R_scope.
-
-Elpi Accumulate File encode.
-
-Definition norm := norm_aux 0%Z 1%Z Z.add Z.mul Z.sub Z.opp Z.eqb.
 
 Elpi Tactic factorize_by_gcd.
 Elpi Accumulate Plugin "ext.elpi".
@@ -20,10 +13,7 @@ Elpi Accumulate lp:{{
 
 solve (goal _ _ _ _ [trm N, trm D] as G) GL :-
   gcd_and_factors N D N' D' Gcd LCM,
-  coq.reduction.vm.norm {{norm lp:N'}} {{Pol Z}} N'',
-  coq.reduction.vm.norm {{norm lp:D'}} {{Pol Z}} D'',
-  coq.reduction.vm.norm {{norm lp:Gcd}} {{Pol Z}} Gcd',
-  (refine {{pair lp:LCM (pair lp:N'' (pair lp:D'' lp:Gcd'))}} G GL).
+  (refine {{pair lp:LCM (pair lp:N' (pair lp:D' lp:Gcd))}} G GL).
 }}.
 
 Definition RField_lemma5 :=
